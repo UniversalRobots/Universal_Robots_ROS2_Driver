a32dcc0ea6630c689a433dc597db2d64e53cb250
John Morris <john@zultron.com>
2020-11-06 12:18:17 -0600

    Add `.dir-locals.el` setting `cmake-mode` vars
    Set emacs `cmake-mode` variables to match `ament_lint_cmake` settings

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+(
+ (nil . ())
+ ;; Silence `ament_lint_cmake` errors
+ (cmake-mode . (
+                ;; Tab found; please use spaces [whitespace/tabs]
+                (indent-tabs-mode . nil)
+                ;; Lines should be <= 80 characters long [linelength]
+                (fill-column . 80)
+                ;; Line ends in whitespace [whitespace/eol]
+                (show-trailing-whitespace . t)
+                ))
+ )
