79cd4f234f3c8d7463a5a95c2d2ad50e2f904635
AndyZe <zelenak@picknik.ai>
2020-11-10 09:02:03 -0600

    Hardware interface framework (#3)
    * Updating ur_robot_driver include guards and namespaces
    
    * Compile the ros_control plugin, inherit from components::SystemInterface
    
    * Do away with intermediate SystemInterface class
    
    * Implement configure()
    
    * Add controller_manager launch file
    
    * Cleanup and clang format

--- ur_robot_driver/include/ur_robot_driver/hardware_interface.h
+++ ur_robot_driver/include/ur_robot_driver/hardware_interface.h
@@ -24,20 +24,58 @@
  *
  */
 //----------------------------------------------------------------------
-#ifndef UR_DRIVER_HARDWARE_INTERFACE_H_INCLUDED
-#define UR_DRIVER_HARDWARE_INTERFACE_H_INCLUDED
+#pragma once
 
-namespace ur_driver
+// System
+#include <memory>
+#include <vector>
+
+// ros2_control hardware_interface
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/components/actuator.hpp"
+#include "hardware_interface/components/sensor.hpp"
+#include "hardware_interface/components/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_status_values.hpp"
+#include "hardware_interface/visibility_control.h"
+
+// ROS
+#include "rclcpp/macros.hpp"
+
+using hardware_interface::HardwareInfo;
+using hardware_interface::return_type;
+using hardware_interface::status;
+using hardware_interface::components::Actuator;
+using hardware_interface::components::Sensor;
+
+namespace ur_robot_driver
 {
 /*!
  * \brief The HardwareInterface class handles the interface between the ROS system and the main
  * driver. It contains the read and write methods of the main control loop and registers various ROS
  * topics and services.
  */
-class HardwareInterface
+class URHardwareInterface : public hardware_interface::components::SystemInterface
 {
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(URHardwareInterface);
+
+  hardware_interface::return_type configure(const HardwareInfo& system_info) final;
+
+  status get_status() const final
+  {
+    return status_;
+  }
+
+  return_type start() final;
+  return_type stop() final;
+  return_type read() final;
+  return_type write() final;
+
+protected:
+  HardwareInfo info_;
+  status status_;
+  std::vector<double> joint_angle_commands_, current_joint_angles_;
 };
 
-}  // namespace ur_driver
-
-#endif  // ifndef UR_DRIVER_HARDWARE_INTERFACE_H_INCLUDED
+}  // namespace ur_robot_driver
