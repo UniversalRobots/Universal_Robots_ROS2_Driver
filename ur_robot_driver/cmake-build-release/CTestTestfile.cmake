7511a2350a43a66cf04583a847ccfa0db491c3b1
Lovro <lovro.ivanov@gmail.com>
2020-11-13 18:15:07 +0000

    Remove build folder.

--- ur_robot_driver/cmake-build-release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catkin_ws/src/Universal_Robots_ROS2_Driver/ur_robot_driver
-# Build directory: /home/catkin_ws/src/Universal_Robots_ROS2_Driver/ur_robot_driver/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
