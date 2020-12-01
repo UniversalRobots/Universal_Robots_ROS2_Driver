1ce877cb8680fea6090fd24674faf1901ebafcc2
livanov93 <lovro.ivanov@gmail.com>
2020-12-01 05:21:10 +0100

    Make xacro property propagate to robot_description (#9)
    * Add robot_description parsing via xacro in launch file. Remove decimal places for limits.
    
    * Remove old commented urdf parsing.
    
    * Remove empty line.
    
    * Change xacro files.
    
    * Adapt launch file to new urdf folder.
    
    * Alphabetize Python imports
    
    Co-authored-by: AndyZe <zelenak@picknik.ai>

--- ur_description/launch/view_ur5.launch.py
+++ ur_description/launch/view_ur5.launch.py
@@ -1,9 +1,9 @@
 import os
+import xacro
 import yaml
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-
 
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
@@ -18,9 +18,9 @@ def load_file(package_name, file_path):
 
 def generate_launch_description():
 
-    # Get URDF
-    robot_description_config = load_file(
-        'ur_description', 'urdf/ur5.urdf.xacro')
+    # Get URDF via xacro
+    robot_description_path = os.path.join(get_package_share_directory('ur_description'), 'urdf', 'ur5_robot.urdf.xacro')
+    robot_description_config = xacro.process_file(robot_description_path).toxml()
     robot_description = {'robot_description': robot_description_config}
 
     robot_description_semantic_config = load_file('ur5_moveit_config', 'config/ur5.srdf')
