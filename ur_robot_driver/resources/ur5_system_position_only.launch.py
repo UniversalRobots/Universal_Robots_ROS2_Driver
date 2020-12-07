abf8e4f9e88e4000471bee4a87a3ba1d36eaa49f
Lovro <lovro.ivanov@gmail.com>
2020-12-07 16:38:30 +0000

    Update test launch file.

--- ur_robot_driver/resources/ur5_system_position_only.launch.py
+++ ur_robot_driver/resources/ur5_system_position_only.launch.py
@@ -21,34 +21,54 @@ from launch_ros.actions import Node
 
 import xacro
 
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 def generate_launch_description():
 
     ld = LaunchDescription()
 
-# TODO(anyone): This does not work... why?
-#    robot_description_file = os.path.join(
-#        get_package_share_directory('ros2_control_demo_robot'),
-#        'description',
-#        'rrbot_system_position_only.urdf.xacro'
-#        )
-#    descr = xacro.process_file(robot_description_file)
-
-    robot_description_file = os.path.join(
+    # Get URDF via xacro
+    robot_description_path = os.path.join(
         get_package_share_directory('ur_robot_driver'),
         'resources',
-        'ur5.urdf'
-        )
-    with open(robot_description_file, 'r') as infile:
-        descr = infile.read()
+        'ur5.urdf.xacro')
+    robot_description_config = xacro.process_file(robot_description_path)
+    robot_description = {'robot_description': robot_description_config.toxml()}
 
-    robot_description = {'robot_description': descr}
+    robot_description_semantic_config = load_file('ur5_moveit_config', 'config/ur5.srdf')
+    robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
 
     ur5_controller = os.path.join(
         get_package_share_directory('ur_robot_driver'),
         'resources',
         'ur5_system_position_only.yaml'
         )
+    # Publishes tf's for the robot
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    # RViz
+    rviz_config_file = get_package_share_directory(
+        'ur_robot_driver') + "/resources/config.rviz"
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     name='rviz2',
+                     output='log',
+                     arguments=['-d', rviz_config_file],
+                     parameters=[robot_description, robot_description_semantic]
+                     )
 
     return LaunchDescription([
       Node(
@@ -59,6 +79,5 @@ def generate_launch_description():
           'stdout': 'screen',
           'stderr': 'screen',
           },
-        )
-
+        ), rviz_node, robot_state_pub_node
     ])
