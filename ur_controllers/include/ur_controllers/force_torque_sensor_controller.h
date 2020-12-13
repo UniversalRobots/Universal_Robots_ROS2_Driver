266ed811a1c00e6332fe17c59841c94e684378aa
Lovro <lovro.ivanov@gmail.com>
2020-12-13 19:06:17 +0000

    Claim individual resources.

--- ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
+++ ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
@@ -18,32 +18,32 @@
 
 namespace ur_controllers
 {
-    class ForceTorqueStateController : public controller_interface::ControllerInterface
-    {
-    public:
-        ForceTorqueStateController();
+class ForceTorqueStateController : public controller_interface::ControllerInterface
+{
+public:
+  ForceTorqueStateController();
 
-        controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-        controller_interface::return_type update() override;
+  controller_interface::return_type update() override;
 
-        CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
 
-        CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
 
-        CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
-    protected:
-        bool init_sensor_data();
-        void init_sensor_state_msg();
+protected:
+  bool init_sensor_data();
+  void init_sensor_state_msg();
 
-        //  we store the name of values per axis - compatible interfaces
-        std::vector<std::string> axis_val_names_;
-        std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::WrenchStamped>> wrench_state_publisher_;
-        geometry_msgs::msg::WrenchStamped wrench_state_msg_;
-    };
+  //  we store the name of values per axis - compatible interfaces
+  std::vector<std::string> axis_val_names_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::WrenchStamped>> wrench_state_publisher_;
+  geometry_msgs::msg::WrenchStamped wrench_state_msg_;
+};
 }  // namespace ur_controllers
 
 #endif  // UR_CONTROLLERS_FORCE_TORQUE_SENSOR_CONTROLLER_H
