472d7502438164d17e5ad20f1e0af7b234432cbf
Lovro <lovro.ivanov@gmail.com>
2021-01-20 21:00:59 +0000

    Update ft state controller with ros2_control changes.

--- ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
+++ ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
@@ -11,8 +11,6 @@
 #include <vector>
 
 #include <controller_interface/controller_interface.hpp>
-#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 
@@ -35,13 +33,15 @@ public:
 
   CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
+    controller_interface::return_type init(const std::string &controller_name) override;
+
 protected:
   bool init_sensor_data();
   void init_sensor_state_msg();
 
   //  we store the name of values per axis - compatible interfaces
   std::vector<std::string> axis_val_names_;
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::WrenchStamped>> wrench_state_publisher_;
+  std::shared_ptr<rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>> wrench_state_publisher_;
   geometry_msgs::msg::WrenchStamped wrench_state_msg_;
 };
 }  // namespace ur_controllers
