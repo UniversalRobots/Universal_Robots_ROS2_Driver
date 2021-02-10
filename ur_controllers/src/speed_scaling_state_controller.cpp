d19c3e2fa80c96933c8305aa3f34647158cae822
Marvin Große Besselmann <grosse@fzi.de>
2021-02-10 19:27:01 +0100

    Code formating and cleanup

--- ur_controllers/src/speed_scaling_state_controller.cpp
+++ ur_controllers/src/speed_scaling_state_controller.cpp
@@ -53,30 +53,26 @@ class State;
 namespace ur_controllers
 {
 const auto kUninitializedValue = std::numeric_limits<double>::quiet_NaN();
+using hardware_interface::HW_IF_EFFORT;
 using hardware_interface::HW_IF_POSITION;
 using hardware_interface::HW_IF_VELOCITY;
-using hardware_interface::HW_IF_EFFORT;
-
 
 SpeedScalingStateController::SpeedScalingStateController()
-{}
-
-controller_interface::InterfaceConfiguration SpeedScalingStateController::command_interface_configuration()
-const
 {
-  return controller_interface::InterfaceConfiguration{controller_interface::
-    interface_configuration_type::NONE};
 }
 
-controller_interface::InterfaceConfiguration SpeedScalingStateController::state_interface_configuration()
-const
+controller_interface::InterfaceConfiguration SpeedScalingStateController::command_interface_configuration() const
 {
-  return controller_interface::InterfaceConfiguration{controller_interface::
-    interface_configuration_type::ALL};
+  return controller_interface::InterfaceConfiguration{ controller_interface::interface_configuration_type::NONE };
+}
+
+controller_interface::InterfaceConfiguration SpeedScalingStateController::state_interface_configuration() const
+{
+  return controller_interface::InterfaceConfiguration{ controller_interface::interface_configuration_type::ALL };
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-SpeedScalingStateController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+SpeedScalingStateController::on_configure(const rclcpp_lifecycle::State& /*previous_state*/)
 {
   try
   {
@@ -93,9 +89,10 @@ SpeedScalingStateController::on_configure(const rclcpp_lifecycle::State & /*prev
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-SpeedScalingStateController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+SpeedScalingStateController::on_activate(const rclcpp_lifecycle::State& /*previous_state*/)
 {
-  if (!init_joint_data()) {
+  if (!init_joint_data())
+  {
     return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
   }
 
@@ -103,20 +100,20 @@ SpeedScalingStateController::on_activate(const rclcpp_lifecycle::State & /*previ
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-SpeedScalingStateController::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+SpeedScalingStateController::on_deactivate(const rclcpp_lifecycle::State& /*previous_state*/)
 {
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
-template<typename T>
-bool has_any_key(
-  const std::unordered_map<std::string, T> & map,
-  const std::vector<std::string> & keys)
+template <typename T>
+bool has_any_key(const std::unordered_map<std::string, T>& map, const std::vector<std::string>& keys)
 {
   bool found_key = false;
-  for (const auto & key_item : map) {
-    const auto & key = key_item.first;
-    if (std::find(keys.cbegin(), keys.cend(), key) != keys.cend()) {
+  for (const auto& key_item : map)
+  {
+    const auto& key = key_item.first;
+    if (std::find(keys.cbegin(), keys.cend(), key) != keys.cend())
+    {
       found_key = true;
       break;
     }
@@ -127,21 +124,24 @@ bool has_any_key(
 bool SpeedScalingStateController::init_joint_data()
 {
   // loop in reverse order, this maintains the order of values at retrieval time
-  for (auto si = state_interfaces_.crbegin(); si != state_interfaces_.crend(); si++) {
+  for (auto si = state_interfaces_.crbegin(); si != state_interfaces_.crend(); si++)
+  {
     // initialize map if name is new
-    if (name_if_value_mapping_.count(si->get_name()) == 0) {
+    if (name_if_value_mapping_.count(si->get_name()) == 0)
+    {
       name_if_value_mapping_[si->get_name()] = {};
     }
     // add interface name
-    name_if_value_mapping_[si->get_name()][si->get_interface_name()] =
-      kUninitializedValue;
+    name_if_value_mapping_[si->get_name()][si->get_interface_name()] = kUninitializedValue;
   }
 
   // filter state interfaces that have a speed scaling factor
   // the rest will be ignored for this message
-  for (const auto & name_ifv : name_if_value_mapping_) {
-    const auto & interfaces_and_values = name_ifv.second;
-    if (has_any_key(interfaces_and_values, {"speed_scaling_factor"})) {
+  for (const auto& name_ifv : name_if_value_mapping_)
+  {
+    const auto& interfaces_and_values = name_ifv.second;
+    if (has_any_key(interfaces_and_values, { "speed_scaling_factor" }))
+    {
       joint_names_.push_back(name_ifv.first);
     }
   }
@@ -152,23 +152,22 @@ bool SpeedScalingStateController::init_joint_data()
 double get_value(const std::unordered_map<std::string, std::unordered_map<std::string, double>>& map,
                  const std::string& name, const std::string& interface_name)
 {
-double get_value(
-  const std::unordered_map<std::string, std::unordered_map<std::string, double>> & map,
-  const std::string & name, const std::string & interface_name)
-{
-  const auto & interfaces_and_values = map.at(name);
+  const auto& interfaces_and_values = map.at(name);
   const auto interface_and_value = interfaces_and_values.find(interface_name);
-  if (interface_and_value != interfaces_and_values.cend()) {
+  if (interface_and_value != interfaces_and_values.cend())
+  {
     return interface_and_value->second;
-  } else {
+  }
+  else
+  {
     return kUninitializedValue;
   }
 }
 
-controller_interface::return_type
-SpeedScalingStateController::update()
+controller_interface::return_type SpeedScalingStateController::update()
 {
-  for (const auto & state_interface : state_interfaces_) {
+  for (const auto& state_interface : state_interfaces_)
+  {
     name_if_value_mapping_[state_interface.get_name()][state_interface.get_interface_name()] =
         state_interface.get_value();
     RCLCPP_DEBUG(get_node()->get_logger(), "%s/%s: %f\n", state_interface.get_name().c_str(),
@@ -186,9 +185,8 @@ SpeedScalingStateController::update()
   return controller_interface::return_type::SUCCESS;
 }
 
-}  // namespace joint_state_controller
+}  // namespace ur_controllers
 
 #include "pluginlib/class_list_macros.hpp"
 
-PLUGINLIB_EXPORT_CLASS(
-  ur_controllers::SpeedScalingStateController, controller_interface::ControllerInterface)
+PLUGINLIB_EXPORT_CLASS(ur_controllers::SpeedScalingStateController, controller_interface::ControllerInterface)
