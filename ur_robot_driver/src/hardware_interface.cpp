3216f06da232a0ac7c452ebc68d225917b88dca5
Lovro <lovro.ivanov@gmail.com>
2021-03-07 11:51:52 +0000

    Fix ci.

--- ur_robot_driver/src/hardware_interface.cpp
+++ ur_robot_driver/src/hardware_interface.cpp
@@ -604,7 +604,8 @@ void URPositionHardwareInterface::checkAsyncIO()
             static_cast<uint8_t>(i - 16), static_cast<bool>(standard_dig_out_bits_cmd_[i]));
       }
 
-      if (io_async_success_ != 2.0) return;
+      if (io_async_success_ != 2.0)
+        return;
     }
   }
 
@@ -612,10 +613,10 @@ void URPositionHardwareInterface::checkAsyncIO()
   {
     if (standard_analog_output_cmd_[i] != standard_analog_output_[i])
     {
-      io_async_success_ = ur_driver_->getRTDEWriter().sendStandardAnalogOutput(i,
-                                                                               standard_analog_output_cmd_[i]);
+      io_async_success_ = ur_driver_->getRTDEWriter().sendStandardAnalogOutput(i, standard_analog_output_cmd_[i]);
 
-        if (io_async_success_ != 2.0) return;
+      if (io_async_success_ != 2.0)
+        return;
     }
   }
 
