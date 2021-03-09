4e7608639fba0a8f4199b73bcf526e8122667044
Lovro <lovro.ivanov@gmail.com>
2021-03-09 18:59:45 +0000

    Publish data of interest.

--- ur_robot_driver/src/hardware_interface.cpp
+++ ur_robot_driver/src/hardware_interface.cpp
@@ -623,8 +623,7 @@ void URPositionHardwareInterface::checkAsyncIO()
     }
   }
 
-  if (speed_scaling_cmd_ != speed_scaling_ && speed_scaling_cmd_ >= 0.01 && speed_scaling_cmd_ <= 1.0 &&
-      ur_driver_ != nullptr)
+  if (speed_scaling_cmd_ != speed_scaling_ &&  ur_driver_ != nullptr)
   {
     scaling_async_success_ = ur_driver_->getRTDEWriter().sendSpeedSlider(speed_scaling_cmd_);
   }
