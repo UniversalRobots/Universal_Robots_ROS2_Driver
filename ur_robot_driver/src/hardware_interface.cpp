343f566fcafa725b4814d49c4fe8415bf809b089
Marvin Große Besselmann <grosse@fzi.de>
2021-04-08 15:53:19 +0200

    Quickfix against move home bug

--- ur_robot_driver/src/hardware_interface.cpp
+++ ur_robot_driver/src/hardware_interface.cpp
@@ -449,6 +449,8 @@ return_type URPositionHardwareInterface::write()
     if (position_interface_in_use_)
     {
       ur_driver_->writeJointCommand(urcl_position_commands_, urcl::comm::ControlMode::MODE_SERVOJ);
+      // remember old values
+      urcl_position_commands_old_ = urcl_position_commands_;
     }
     else
     {
@@ -456,9 +458,6 @@ return_type URPositionHardwareInterface::write()
     }
 
     packet_read_ = false;
-
-    // remember old values
-    urcl_position_commands_old_ = urcl_position_commands_;
 
     return return_type::OK;
   }
@@ -473,7 +472,8 @@ void URPositionHardwareInterface::handleRobotProgramState(bool program_running)
 {
   if (!robot_program_running_ && program_running)
   {
-    // TODO how to set controller reset flag
+    urcl_position_commands_old_ = urcl_position_commands_;
+    position_interface_in_use_ = false;
   }
   robot_program_running_ = program_running;
 }
