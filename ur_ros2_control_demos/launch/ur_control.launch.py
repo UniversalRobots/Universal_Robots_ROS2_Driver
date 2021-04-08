fb98abdfad6311fb1b7b7e59b887f1aff3303e92
Denis Stogl <denis.stogl@picknik.ai>
2021-04-08 18:27:41 +0200

    Add launch for all version of UR robots

--- ur_ros2_control_demos/launch/ur_control.launch.py
+++ ur_ros2_control_demos/launch/ur_control.launch.py
@@ -14,151 +14,265 @@
 #
 # Author: Denis Stogl
 
-import os
-
-from ament_index_python.packages import get_package_prefix, get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-
-import xacro
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     declared_arguments = []
-    declared_arguments.append(DeclareLaunchArgument(
-        'ur_type',
-        description='Type/series of used UR robot.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'robot_ip',
-        description='IP address by which the robot can be reached.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'safety_limits', default_value='true',
-        description='Enables the safety limits controller if true.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'safety_pos_margin', default_value='0.15',
-        description='The margin to lower and upper limits in the safety controller.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'safety_k_position', default_value='20',
-        description='k-position factor in the safety controller.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'use_fake_hardware', default_value='false',
-        description='Start robot with fake hardware mirroring command to its states.'))
-    declared_arguments.append(DeclareLaunchArgument(
-        'fake_sensor_commands', default_value='false',
-        description='Enable fake command interfaces for sensors used for simple simulations. \
-            Used only if \'use_fake_hardware\' parameter is true.'))
-
-    ur_type = LaunchConfiguration('ur_type')
-    robot_ip = LaunchConfiguration('robot_ip')
-    safety_limits = LaunchConfiguration('safety_limits')
-    safety_pos_margin = LaunchConfiguration('safety_pos_margin')
-    safety_k_position = LaunchConfiguration('safety_k_position')
-    use_fake_hardware = LaunchConfiguration('use_fake_hardware')
-    use_ros2_control = 'true'
-    fake_sensor_commands = LaunchConfiguration('fake_sensor_commands')
-
-    joint_limit_params = PathJoinSubstitution([
-        get_package_share_directory('ur_description'), 'config', ur_type, 'joint_limits.yaml'])
-    kinematics_params = PathJoinSubstitution([
-        get_package_share_directory('ur_description'), 'config', ur_type,
-        'default_kinematics.yaml'])
-    physical_params = PathJoinSubstitution([
-        get_package_share_directory('ur_description'), 'config', ur_type,
-        'physical_parameters.yaml'])
-    visual_params = PathJoinSubstitution([
-        get_package_share_directory('ur_description'), 'config', ur_type, 'visual_parameters.yaml'])
-    script_filename = PathJoinSubstitution([
-        get_package_share_directory('ur_robot_driver'), 'resources', 'ros_control.urscript'])
-    input_recipe_filename = PathJoinSubstitution([
-        get_package_share_directory('ur_robot_driver'), 'resources', 'rtde_input_recipe.txt'])
-    output_recipe_filename = PathJoinSubstitution([
-        get_package_share_directory('ur_robot_driver'), 'resources', 'rtde_output_recipe.txt'])
-
-    robot_description_content = Command([
-        PathJoinSubstitution([get_package_prefix('xacro'), 'bin', 'xacro']),
-        ' ',
-        PathJoinSubstitution([get_package_share_directory('ur_description'), 'urdf', 'ur.xacro']),
-        ' ',
-        'robot_ip:=', robot_ip, ' ',
-        'joint_limit_params:=', joint_limit_params, ' ',
-        'kinematics_params:=', kinematics_params, ' ',
-        'physical_params:=', physical_params, ' ',
-        'visual_params:=', visual_params, ' ',
-        'safety_limits:=', safety_limits, ' ',
-        'safety_pos_margin:=', safety_pos_margin, ' ',
-        'safety_k_position:=', safety_k_position, ' ',
-        'name:=', ur_type, ' ',
-        'use_ros2_control:=', use_ros2_control, ' ',
-        'script_filename:=', script_filename, ' ',
-        'input_recipe_filename:=', input_recipe_filename, ' ',
-        'output_recipe_filename:=', output_recipe_filename, ' ',
-        ])
-    robot_description = {'robot_description': robot_description_content}
-
-    ur_controllers = PathJoinSubstitution([
-      get_package_share_directory('ur_ros2_control_demos'), 'config', 'ur_controllers.yaml'])
-
-    # Publishes tf's for the robot
-    robot_state_pub_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument("ur_type", description="Type/series of used UR robot.")
+    )
+    # TODO(anyone): enable this when added into ROS2-foxy
+    # choices=['ur3', 'ur3e', 'ur5', 'ur5e', 'ur10', 'ur10e', 'ur16e']))
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip", description="IP address by which the robot can be reached."
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="ur_ros2_control_demos",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ur_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="ur_description",
+            description="Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="ur.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
     )
 
-    # RViz
-    rviz_config_file = PathJoinSubstitution([
-        get_package_share_directory('ur_ros2_control_demos'), 'config', 'rviz', 'config.rviz'])
-    rviz_node = Node(package='rviz2',
-                     executable='rviz2',
-                     name='rviz2',
-                     output='log',
-                     arguments=['-d', rviz_config_file],
-                     parameters=[robot_description]
-                     # parameters=[robot_description, robot_description_semantic]
-                     )
+    # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    robot_ip = LaunchConfiguration("robot_ip")
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    # General arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    prefix = LaunchConfiguration("prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    robot_controller = LaunchConfiguration("robot_controller")
 
-    ros2_control_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[robot_description, ur_controllers],
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "visual_parameters.yaml"]
+    )
+    script_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"]
+    )
+    input_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
+    )
+    output_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
+    )
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            " ",
+            "robot_ip:=",
+            robot_ip,
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            ur_type,
+            " ",
+            "script_filename:=",
+            script_filename,
+            " ",
+            "input_recipe_filename:=",
+            input_recipe_filename,
+            " ",
+            "output_recipe_filename:=",
+            output_recipe_filename,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "fake_sensor_commands:=",
+            fake_sensor_commands,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare(runtime_config_package), "config", controllers_file]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
         output={
-            'stdout': 'screen',
-            'stderr': 'screen',
+            "stdout": "screen",
+            "stderr": "screen",
         },
     )
 
-    dashboard_client_node = Node(
-        package="ur_robot_driver",
-        executable="dashboard_client",
-        name="dashboard_client",
-        output="screen",
-        emulate_tty=True,
-        parameters=[
-            {"robot_ip": robot_ip}
-        ]
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
     )
 
-    return LaunchDescription(
-        declared_arguments,
-        [
-            robot_state_pub_node,
-            rviz_node,
-            ros2_control_node,
-            dashboard_client_node
-        ])
+    joint_state_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_state_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=[robot_controller, "-c", "/controller_manager"],
+    )
+
+    nodes_to_start = [
+        control_node,
+        robot_state_pub_node,
+        rviz_node,
+        joint_state_controller_spawner,
+        robot_controller_spawner,
+    ]
+
+    if use_fake_hardware == "false":
+
+        dashboard_client_node = Node(
+            package="ur_robot_driver",
+            executable="dashboard_client",
+            name="dashboard_client",
+            output="screen",
+            emulate_tty=True,
+            parameters=[{"robot_ip": robot_ip}],
+        )
+        nodes_to_start.append(dashboard_client_node)
+
+    return LaunchDescription(declared_arguments + nodes_to_start)
