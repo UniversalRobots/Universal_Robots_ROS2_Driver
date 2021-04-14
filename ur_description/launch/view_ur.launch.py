5777d124bab78b8b3009198f6f73c0cfe1e35433
Denis Štogl <destogl@users.noreply.github.com>
2021-04-14 08:59:35 +0200

    Added view_ur for checking description (#81)
    * Added view_ur for checking description

--- ur_description/launch/view_ur.launch.py
+++ ur_description/launch/view_ur.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, PickNik, Inc.
+# Copyright (c) 2021 PickNik, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,103 +10,180 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.v
+# limitations under the License.
+#
+# Author: Denis Stogl
 
-import os
-
-import xacro
-import yaml
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-
-    # set ur robot
-    robot_name = "ur5_e"
-
-    # <robot_name> parameters files
-    joint_limits_params = os.path.join(
-        get_package_share_directory("ur_description"),
-        "config/" + robot_name.replace("_", ""),
-        "joint_limits.yaml",
+    declared_arguments = []
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument("ur_type", description="Type/series of used UR robot.")
     )
-    kinematics_params = os.path.join(
-        get_package_share_directory("ur_description"),
-        "config/" + robot_name.replace("_", ""),
-        "default_kinematics.yaml",
+    # TODO(anyone): enable this when added into ROS2-foxy
+    # choices=['ur3', 'ur3e', 'ur5', 'ur5e', 'ur10', 'ur10e', 'ur16e']))
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
     )
-    physical_params = os.path.join(
-        get_package_share_directory("ur_description"),
-        "config/" + robot_name.replace("_", ""),
-        "physical_parameters.yaml",
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        )
     )
-    visual_params = os.path.join(
-        get_package_share_directory("ur_description"),
-        "config/" + robot_name.replace("_", ""),
-        "visual_parameters.yaml",
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="ur_bringup",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ur_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="ur_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="ur.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
     )
 
-    # common parameters
-    # If True, enable the safety limits controller
-    safety_limits = False
-    # The lower/upper limits in the safety controller
-    safety_pos_margin = 0.15
-    # Used to set k position in the safety controller
-    safety_k_position = 20
+    # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    # General arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    prefix = LaunchConfiguration("prefix")
 
-    # Get URDF via xacro
-    robot_description_path = os.path.join(
-        get_package_share_directory("ur_description"), "urdf", "ur.xacro"
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "visual_parameters.yaml"]
+    )
+    script_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"]
+    )
+    input_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
+    )
+    output_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
     )
 
-    robot_description_config = xacro.process_file(
-        robot_description_path,
-        mappings={
-            "joint_limit_params": joint_limits_params,
-            "kinematics_params": kinematics_params,
-            "physical_params": physical_params,
-            "visual_params": visual_params,
-            "safety_limits": str(safety_limits).lower(),
-            "safety_pos_margin": str(safety_pos_margin),
-            "safety_k_position": str(safety_k_position),
-            "name": robot_name.replace("_", ""),
-        },
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            ur_type,
+            " ",
+            "script_filename:=",
+            script_filename,
+            " ",
+            "input_recipe_filename:=",
+            input_recipe_filename,
+            " ",
+            "output_recipe_filename:=",
+            output_recipe_filename,
+            " ",
+            "prefix:=",
+            prefix,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
     )
 
-    robot_description = {"robot_description": robot_description_config.toxml()}
-
-    robot_description_semantic_config = load_file(
-        robot_name + "_moveit_config", "config/" + robot_name.replace("_", "") + ".srdf"
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
     )
-    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_config}
-
-    # We do not have a robot connected, so publish fake joint states
-    joint_state_pub_node = Node(package="joint_state_publisher", executable="joint_state_publisher")
-
-    # Publishes tf's for the robot
-    robot_state_pub_node = Node(
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="screen",
+        output="both",
         parameters=[robot_description],
-    )
-
-    # RViz
-    rviz_config_file = (
-        get_package_share_directory("ur_description") + "/config/rviz/view_robot.rviz"
     )
     rviz_node = Node(
         package="rviz2",
@@ -114,7 +191,12 @@ def generate_launch_description():
         name="rviz2",
         output="log",
         arguments=["-d", rviz_config_file],
-        parameters=[robot_description, robot_description_semantic],
     )
 
-    return LaunchDescription([rviz_node, robot_state_pub_node, joint_state_pub_node])
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes_to_start)
