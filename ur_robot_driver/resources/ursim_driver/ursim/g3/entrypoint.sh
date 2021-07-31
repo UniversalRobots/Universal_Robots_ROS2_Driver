5ff752c260fb15079535662ea514a8450c18bd85
voodoo <lovro.ivanov@gmail.com>
2021-07-31 22:11:35 +0200

    Fix formatting.

--- ur_robot_driver/resources/ursim_driver/ursim/g3/entrypoint.sh
+++ ur_robot_driver/resources/ursim_driver/ursim/g3/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Setup VNC server
+rm /tmp/.X1-lock && rm /tmp/.X11-unix/X1
 Xvfb :1 -screen 0 800x600x16 &
-x11vnc -bg -quiet -forever -shared -display :1
+x11vnc -create -bg -quiet -forever -shared -display :1
 
 # Execute URSim
 /ursim/start-ursim.sh ${ROBOT_MODEL}
+
