1936e5593a86fb88ff07d466f586d18d301e846c
voodoo <lovro.ivanov@gmail.com>
2021-08-18 19:47:29 +0200

    Add ros2 distro and build moveit arguments.

--- ur_robot_driver/resources/ursim_driver/README.md
+++ ur_robot_driver/resources/ursim_driver/README.md
@@ -6,8 +6,9 @@
 
 ## Driver and URsim
 Driver and URsim can be run together by following command  which will build necessary docker images and start the containers.
+With appropriate arguments set the ROS2 distro and the Moveit2! build is required.
 ```
-docker-compose up
+ROS2_DISTRO=foxy BUILD_MOVEIT2=0 docker-compose up --build
 ```
 
 
@@ -17,49 +18,4 @@ able to see active connections. Use 0.0.0.0:5900 for ursim and 0.0.0.0:5566 for 
 
  ![vncviewer_list](resources/vncviewer_list.png)
 
- Open the URsim vnc connection to install necessary urcap via settings tab.
-
- ![settings](resources/settings.png)
-
- Navigate to URCaps menu.
-
- ![urcap_menu](resources/urcap_menu.png)
-
- Add new urcap for external control option for UR robots. After selection click Open.
-
- ![add_urcap](resources/add_urcap.png)
-
- When the urcap is successfully added the simulator must be restarted.
-
- Open new terminal and enter:
-```
-  docker restart ursimdriver_ursim_1
-```
-
-![added_urcap](resources/added_urcap.png)
-
-Reopen ursim connection and power on the robot by clicking the bottom left red button.
-
-![poweron_proc](resources/poweron_proc.png)
-
-Click ON and after that START button. When the robot is powered on all the LEDs should be green.
-
-![powered_on](resources/powered_on.png)
-
-Add the urcap for external control as active program in the Program tab.
-
-![added_ext_ctrl](resources/added_external_ctrl.png)
-
-Reconnect the driver by restarting its container.
-```
-  docker restart ursimdriver_driver_1
-```
-When the driver container is up and running again click play button in the simulator app. Successful connection
-will result with the following app status and report:
-
-![app_status](resources/running_external_ctrl.png)
-
-![app_report](resources/terminal.png)
-
-## Driver only
-...
+ Open the URsim vnc connection to see the simulator window.
--- ur_robot_driver/resources/ursim_driver/driver/entrypoint.sh
+++ ur_robot_driver/resources/ursim_driver/driver/entrypoint.sh
@@ -5,11 +5,11 @@ rm /tmp/.X1-lock && rm /tmp/.X11-unix/X1
 Xvfb :1 -screen 0 1366x768x16 &
 x11vnc -rfbport 5566 -create -bg -quiet -forever -shared -display :1
 /wait_dashboard_server.sh
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 launch ur_bringup ur_control.launch.py ur_type:=ur5 robot_ip:=192.168.56.101 use_fake_hardware:=false launch_rviz:=false &
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/load_installation ur_dashboard_msgs/srv/Load "{filename: urcap_ros_control.installation}"
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/load_program ur_dashboard_msgs/srv/Load "{filename: urcap_ros_control.urp}"
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/power_on std_srvs/srv/Trigger
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/brake_release std_srvs/srv/Trigger
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/close_popup std_srvs/srv/Trigger
-source /~/workspace/ros_ws_foxy_ur_driver/install/setup.bash && ros2 service call /dashboard_client/play  std_srvs/srv/Trigger
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 launch ur_bringup ur_control.launch.py ur_type:=ur5 robot_ip:=192.168.56.101 use_fake_hardware:=false launch_rviz:=false &
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/load_installation ur_dashboard_msgs/srv/Load "{filename: urcap_ros_control.installation}"
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/load_program ur_dashboard_msgs/srv/Load "{filename: urcap_ros_control.urp}"
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/power_on std_srvs/srv/Trigger
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/brake_release std_srvs/srv/Trigger
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/close_popup std_srvs/srv/Trigger
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/play  std_srvs/srv/Trigger
 tail -F anything
