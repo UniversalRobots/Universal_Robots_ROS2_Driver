ef1e967b9065b6a9006b1e576581cbafbdd70c08
root <lovro.ivanov@gmail.com>
2021-09-22 12:30:01 +0000

    Remove local build files.

--- ur_calibration/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/ros_ws_ur_driver/src/Universal_Robots_ROS2_Driver/ur_calibration
-# Build directory: /home/workspace/ros_ws_ur_driver/src/Universal_Robots_ROS2_Driver/ur_calibration/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
