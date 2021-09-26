8c1e7f82e561acc11f35709c2f7b17b3170c45a5
AndyZe <zelenak@picknik.ai>
2021-09-25 22:37:53 -0500

    Launch the velocity controller by default

--- ur_bringup/launch/ur_control.launch.py
+++ ur_bringup/launch/ur_control.launch.py
@@ -114,7 +114,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "robot_controller",
-            default_value="joint_trajectory_controller",
+            default_value="forward_velocity_controller",
             description="Robot controller to start.",
         )
     )
