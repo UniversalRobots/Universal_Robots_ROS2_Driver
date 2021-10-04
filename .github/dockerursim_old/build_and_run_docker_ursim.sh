2fadb6fb1efabddcc4c2a0ebddba429b98854373
root <lovro.ivanov@gmail.com>
2021-10-04 11:36:02 +0000

    Add new dockerursim configuration.

--- .github/dockerursim_old/build_and_run_docker_ursim.sh
+++ .github/dockerursim_old/build_and_run_docker_ursim.sh
@@ -4,20 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker network create --subnet=192.168.0.0/16 static_test_net
 
-docker build ${DIR} -t mydockerursim \
-  --build-arg URSIM="https://s3-eu-west-1.amazonaws.com/ur-support-site/118926/URSim_Linux-5.11.1.108318.tar.gz" \
-  --build-arg VERSION=5.11.1.108318
+docker build ${DIR} -t mydockerursim
 docker volume create dockerursim
 docker run --name="mydockerursim" -d \
   -e ROBOT_MODEL=UR5 \
-  -e DISPLAY=:1 \
   --net static_test_net \
   --ip 192.168.56.101 \
-  -p 5900:5900 \
   -p 8080:8080 \
   -p 29999:29999 \
   -p 30001-30004:30001-30004 \
-  -v "${DIR}/../aux/.vol":/ursim/programs \
+  -v "${DIR}/.vol":/ursim/programs \
+  -v dockursim:/ursim \
   --privileged \
   --cpus=1 \
   mydockerursim
