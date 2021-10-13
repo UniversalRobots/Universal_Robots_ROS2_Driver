61c2115d4e0d810b9ad77bc77cd7a3a90f2e1536
AndyZe <zelenak@picknik.ai>
2021-10-13 10:49:33 -0500

    Do not customize the planning scene topics

--- ur_bringup/launch/ur_moveit.launch.py
+++ ur_bringup/launch/ur_moveit.launch.py
@@ -179,15 +179,6 @@ def launch_setup(context, *args, **kwargs):
         "publish_geometry_updates": True,
         "publish_state_updates": True,
         "publish_transforms_updates": True,
-        "planning_scene_monitor_options": {
-            "name": "planning_scene_monitor",
-            "robot_description": "robot_description",
-            "joint_state_topic": "/joint_states",
-            "attached_collision_object_topic": "/move_group/planning_scene_monitor",
-            "publish_planning_scene_topic": "/move_group/publish_planning_scene",
-            "monitored_planning_scene_topic": "/move_group/monitored_planning_scene",
-            "wait_for_initial_state_timeout": 10.0,
-        },
     }
 
     # Start the actual move_group node/action server
