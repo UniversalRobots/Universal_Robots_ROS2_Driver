ae5106fc688fedab99486958b785f861607194f2
Lovro <lovro.ivanov@gmail.com>
2021-10-19 10:37:24 +0200

    Remove stopping step.

--- .github/dockerursim/stop_and_remove_ursim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker stop mydockerursim && docker rm mydockerursim
-docker network create rm static_test_net
