c245bc531bd61e75cb48c405e58bbd6b4f04b131
Lovro <lovro.ivanov@gmail.com>
2021-10-21 14:45:09 +0200

    Pre-commit fixes.

--- ur_robot_driver/scripts/tool_communication.py
+++ ur_robot_driver/scripts/tool_communication.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Small helper script to start the tool communication interface"""
+"""Small helper script to start the tool communication interface."""
 
 import subprocess
 
@@ -8,19 +8,20 @@ from rclpy.node import Node
 
 
 class UrToolCommunication(Node):
-    """Starts socat"""
+    """Starts socat."""
+
     def __init__(self):
-        super().__init__('ur_tool_communication')
+        super().__init__("ur_tool_communication")
         # IP address of the robot
-        self.declare_parameter('robot_ip', "192.168.56.101")
+        self.declare_parameter("robot_ip", "192.168.56.101")
         robot_ip = self.get_parameter("robot_ip").get_parameter_value().string_value
         self.get_logger().info(robot_ip)
         # Port on which the remote pc (robot) publishes the interface
-        self.declare_parameter('tcp_port', "54321")
+        self.declare_parameter("tcp_port", "54321")
         tcp_port = self.get_parameter_or("tcp_port", "54321").get_parameter_value().string_value
         # By default, socat will create a pty in /dev/pts/N with n being an increasing number.
         # Additionally, a symlink at the given location will be created. Use an absolute path here.
-        self.declare_parameter('device_name', "/tmp/ttyUR")
+        self.declare_parameter("device_name", "/tmp/ttyUR")
         local_device = self.get_parameter("device_name").get_parameter_value().string_value
 
         self.get_logger().info("Remote device is available at '" + local_device + "'")
