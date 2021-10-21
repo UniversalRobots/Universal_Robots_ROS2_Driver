9cbde9af903ab7480f0774a9d4b3c7ca9ae0f110
Lovro <lovro.ivanov@gmail.com>
2021-10-21 18:24:43 +0200

    Change robot type.

--- ur_robot_driver/resources/ursim_driver/driver/entrypoint.sh
+++ ur_robot_driver/resources/ursim_driver/driver/entrypoint.sh
@@ -8,5 +8,5 @@ x11vnc -rfbport 5566 -create -bg -quiet -forever -shared -display :1
 source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 launch ur_bringup ur_dashboard_client.launch.py robot_ip:=192.168.56.101 &
 source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/power_on std_srvs/srv/Trigger
 source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 service call /dashboard_client/brake_release std_srvs/srv/Trigger
-source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 launch ur_bringup ur_control.launch.py headless_mode:=true ur_type:=ur5 robot_ip:=192.168.56.101 use_fake_hardware:=false launch_rviz:=false &
+source /~/workspace/ros_ws_ur_driver/install/setup.bash && ros2 launch ur_bringup ur_control.launch.py headless_mode:=true ur_type:=ur5e robot_ip:=192.168.56.101 use_fake_hardware:=false launch_rviz:=false &
 tail -F anything
