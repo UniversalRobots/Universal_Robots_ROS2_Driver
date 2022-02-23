71d61afbf92d142e091cec72fb95c7a42f2028f1
Denis Štogl <denis@stogl.de>
2022-02-23 10:43:25 +0100

    Define list of default controllers for example launch files. This should enable simpler start because users can see which controllers are defined.

--- ur_bringup/launch/ur10e.launch.py
+++ ur_bringup/launch/ur10e.launch.py
@@ -49,6 +49,12 @@ def generate_launch_description():
             "initial_joint_controller",
             default_value="scaled_joint_trajectory_controller",
             description="Initially loaded robot controller.",
+            choices=[
+                "scaled_joint_trajectory_controller",
+                "joint_trajectory_controller",
+                "forward_velocity_controller",
+                "forward_position_controller",
+            ],
         )
     )
     declared_arguments.append(
