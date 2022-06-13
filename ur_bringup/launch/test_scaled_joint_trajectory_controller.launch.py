ea2c2a0799fd8ce212ed22e1b817256916df5b57
Felix Exner <exner@fzi.de>
2022-06-13 08:51:09 +0200

    Foxy: Update dependencies and binary repos file (#373)
    * Remove upstream dependencies from binary build
    
    * sort package.xml according to schema
    
    * Updated ur_bringup package dependencies
    
    * Add notice about ros2_control_demos
    
    * remove ur_robot_driver dependency from ur_bringup
    
    This is the same as for the main branch and should be resolved separately
    
    * Do not fail fast
    
    * Add an understandable error in case of a missing ros2_control_demos

--- ur_bringup/launch/test_scaled_joint_trajectory_controller.launch.py
+++ ur_bringup/launch/test_scaled_joint_trajectory_controller.launch.py
@@ -15,11 +15,13 @@
 # Author: Denis Stogl
 #
 # Description: After a robot has been loaded, this will execute a series of trajectories.
+import sys
 
 from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
 
 
 def generate_launch_description():
@@ -28,6 +30,20 @@ def generate_launch_description():
         [FindPackageShare("ur_bringup"), "config", "test_goal_publishers_config.yaml"]
     )
 
+    # Kind of hack until this dependency is released
+    # TODO (@anyone): Remove this once the upstream package is released
+    try:
+        get_package_share_directory("ros2_control_test_nodes")
+    except PackageNotFoundError:
+        print(
+            "ERROR:"
+            "Could not find package 'ros2_control_test_nodes'. Please install it (build it from "
+            "source) in order to run this launchfile. See here for details: explanation:\n"
+            "https://github.com/UniversalRobots/Universal_Robots_ROS2_Driver/tree/foxy"
+            "#example-commands-for-testing-the-driver"
+        )
+        sys.exit(1)
+
     return LaunchDescription(
         [
             Node(
@@ -35,10 +51,7 @@ def generate_launch_description():
                 executable="publisher_joint_trajectory_controller",
                 name="publisher_scaled_joint_trajectory_controller",
                 parameters=[position_goals],
-                output={
-                    "stdout": "screen",
-                    "stderr": "screen",
-                },
+                output={"stdout": "screen", "stderr": "screen"},
             )
         ]
     )
