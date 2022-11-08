45a1414a4366fafd8caa3b387b974845b3f4a769
Felix Exner <exner@fzi.de>
2022-11-08 14:30:07 +0000

    Fix capitalization of docstring

--- ur_moveit_config/ur_moveit_config/launch_common.py
+++ ur_moveit_config/ur_moveit_config/launch_common.py
@@ -39,7 +39,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def construct_angle_radians(loader, node):
-    """utility function to construct radian values from yaml."""
+    """Utility function to construct radian values from yaml."""
     value = loader.construct_scalar(node)
     try:
         return float(value)
@@ -48,7 +48,7 @@ def construct_angle_radians(loader, node):
 
 
 def construct_angle_degrees(loader, node):
-    """utility function for converting degrees into radians from yaml."""
+    """Utility function for converting degrees into radians from yaml."""
     return math.radians(construct_angle_radians(loader, node))
 
 
