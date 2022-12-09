4dfeb59552afd1630408334fd1c69251470391f3
Lennart Nachtigall <firesurfer@firesurfer.de>
2022-12-09 10:02:55 +0100

    This commits adds additional configuration parameters needed for multiarm support. Additionally is cleans up the parameters parsing in the on_activate method.
    Added parameters:
    
    - trajectory_port
    - script_command_port
    - keep_alive_count
    
    See Universal_Robots_ROS2_Description for more explainations
    
    Minor comment adaptions
    
    Code formatting
    
    Update prefix -> tf_prefix
    
    Update ur_bringup launchfile, as well
    
    Until we really deprecate ur_bringup, we'll have to keep it up to date.
    
    Update binary build dependencies
    
    Since with recent changes we need the latest description, we should modify the dependencies files, as well.

--- ur_bringup/launch/ur_control.launch.py
+++ ur_bringup/launch/ur_control.launch.py
@@ -51,7 +51,7 @@ def launch_setup(context, *args, **kwargs):
     controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
-    prefix = LaunchConfiguration("prefix")
+    tf_prefix = LaunchConfiguration("tf_prefix")
     use_fake_hardware = LaunchConfiguration("use_fake_hardware")
     fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
@@ -133,8 +133,8 @@ def launch_setup(context, *args, **kwargs):
             "output_recipe_filename:=",
             output_recipe_filename,
             " ",
-            "prefix:=",
-            prefix,
+            "tf_prefix:=",
+            tf_prefix,
             " ",
             "use_fake_hardware:=",
             use_fake_hardware,
@@ -420,10 +420,10 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "prefix",
+            "tf_prefix",
             default_value='""',
             description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        multi-robot setup. If changed, also joint names in the controllers' configuration \
         have to be updated.",
         )
     )
