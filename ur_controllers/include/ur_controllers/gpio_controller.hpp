fb5972dcd28425a73da3aa524493c9bc886f0f2c
Lennart Nachtigall <firesurfer127@gmail.com>
2023-05-17 19:25:50 +0200

    Adding maximum retry counter in gpio controller (Multiarm part 3) - v2 (#672)
    * Add waitForAsyncCommand method in gpio controller which introduces a timeout while waiting for the async command success
    
    Co-authored-by: Lennart Nachtigall <firesurfer@firesurfer.de>

--- ur_controllers/include/ur_controllers/gpio_controller.hpp
+++ ur_controllers/include/ur_controllers/gpio_controller.hpp
@@ -184,6 +184,12 @@ protected:
   // TODO(anyone) publishers to add: tcp_pose_pub_
   // TODO(anyone) subscribers to add: script_command_sub_
   // TODO(anyone) service servers to add: resend_robot_program_srv_, deactivate_srv_, set_payload_srv_, tare_sensor_srv_
+
+  /**
+   * @brief wait until a command interface isn't in state ASYNC_WAITING anymore or until the parameter maximum_retries
+   * have been reached
+   */
+  bool waitForAsyncCommand(std::function<double(void)> get_value);
 };
 }  // namespace ur_controllers
 
