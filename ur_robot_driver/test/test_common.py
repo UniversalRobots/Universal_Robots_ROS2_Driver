0096acac3d170ba01f560a413ed34d27d4e4e4a4
Felix Exner <exner@fzi.de>
2024-04-17 06:24:32 +0200

    Add UR30

--- ur_robot_driver/test/test_common.py
+++ ur_robot_driver/test/test_common.py
@@ -253,7 +253,7 @@ def _declare_launch_arguments():
             "ur_type",
             default_value="ur5e",
             description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20"],
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
         )
     )
 
