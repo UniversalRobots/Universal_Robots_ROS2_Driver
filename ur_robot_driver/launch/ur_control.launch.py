746b9314534b324b63b1a06b38e731d573993524
Chen Chen <maker_cc@foxmail.com>
2024-09-09 20:11:34 +0800

    failure trial, because cannot pass all controllers as a single string separated with space

--- ur_robot_driver/launch/ur_control.launch.py
+++ ur_robot_driver/launch/ur_control.launch.py
@@ -42,6 +42,7 @@ from launch.substitutions import (
     LaunchConfiguration,
     NotSubstitution,
     PathJoinSubstitution,
+    PythonExpression,
 )
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterFile
@@ -145,7 +146,7 @@ def launch_setup(context):
     )
 
     # Spawn controllers
-    def controller_spawner(controllers, active=True):
+    def controller_spawner(controllers, condition, active=True):
         inactive_flags = ["--inactive"] if not active else []
         return Node(
             package="controller_manager",
@@ -157,28 +158,61 @@ def launch_setup(context):
                 controller_spawner_timeout,
             ]
             + inactive_flags
-            + controllers,
+            + [controllers],
+            condition=condition,
         )
 
-    controllers_active = [
+    controllers_active_default = [
         "joint_state_broadcaster",
         "io_and_status_controller",
         "speed_scaling_state_broadcaster",
         "force_torque_sensor_broadcaster",
     ]
-    controllers_inactive = [
+    controllers_inactive_default = [
         "scaled_joint_trajectory_controller",
         "joint_trajectory_controller",
         "forward_velocity_controller",
         "forward_position_controller",
     ]
-    if activate_joint_controller.perform(context) == "true":
-        controllers_active.append(initial_joint_controller.perform(context))
-        controllers_inactive.remove(initial_joint_controller.perform(context))
+    controllers_active_init = PythonExpression(
+        [
+            "'",
+            " ".join(controllers_active_default),
+            "' + ' ' +'",
+            initial_joint_controller,
+            "'",
+        ]
+    )
+    controllers_inactive_init = PythonExpression(
+        [
+            "' '.join([c for c in ",
+            str(controllers_inactive_default),
+            "if c != '",
+            initial_joint_controller,
+            "'])",
+        ]
+    )
+    print(controllers_active_init.perform(context))
+    print(controllers_inactive_init.perform(context))
 
     controller_spawners = [
-        controller_spawner(controllers_active),
-        controller_spawner(controllers_inactive, active=False),
+        controller_spawner(
+            controllers_active_init, condition=IfCondition(activate_joint_controller)
+        ),
+        controller_spawner(
+            controllers_inactive_init,
+            condition=IfCondition(activate_joint_controller),
+            active=False,
+        ),
+        controller_spawner(
+            " ".join(controllers_active_default),
+            condition=UnlessCondition(activate_joint_controller),
+        ),
+        controller_spawner(
+            " ".join(controllers_inactive_default),
+            condition=UnlessCondition(activate_joint_controller),
+            active=False,
+        ),
     ]
 
     rsp = IncludeLaunchDescription(
