47b293dba16fc6efc127331cc58b60ffbc682b72
Felix Exner <feex@universal-robots.com>
2025-05-15 11:08:04 +0200

    Add support for launching a UR15 robot (#1359)

--- ur_bringup/launch/ur_control.launch.py
+++ ur_bringup/launch/ur_control.launch.py
@@ -60,6 +60,7 @@ def generate_launch_description():
                 "ur10e",
                 "ur12e",
                 "ur16e",
+                "ur15",
                 "ur20",
                 "ur30",
             ],
