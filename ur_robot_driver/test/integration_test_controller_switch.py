3201f733fcde827cd6d1f62d651a214eeefd0d30
Jacob Larsen <jala@universal-robots.com>
2025-07-30 10:32:55 +0000

    Apply renaming to the last few test classes

--- ur_robot_driver/test/integration_test_controller_switch.py
+++ ur_robot_driver/test/integration_test_controller_switch.py
@@ -65,12 +65,12 @@ def generate_test_description(tf_prefix):
     return generate_driver_test_description(tf_prefix=tf_prefix)
 
 
-class RobotDriverTest(unittest.TestCase):
+class ControllerSwitchTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Initialize the ROS context
         rclpy.init()
-        cls.node = Node("robot_driver_test")
+        cls.node = Node("controller_switching_test")
         time.sleep(1)
         cls.init_robot(cls)
 
