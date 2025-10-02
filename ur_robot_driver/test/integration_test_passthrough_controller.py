229f045b580114a0bfc0dbd81090ea925f2993f9
Felix Exner <feex@universal-robots.com>
2025-10-02 12:14:48 +0000

    Fix typo

--- ur_robot_driver/test/integration_test_passthrough_controller.py
+++ ur_robot_driver/test/integration_test_passthrough_controller.py
@@ -109,7 +109,7 @@ class PassthroughControllerTest(unittest.TestCase):
 
         # Wait for all controllers needed below, as controller manager services might fail
         # e.g. when attempting to deactivate an unknown controller
-        self._controller_manager_interface.wait_for_controlle("scaled_joint_trajectory_controller")
+        self._controller_manager_interface.wait_for_controller("scaled_joint_trajectory_controller")
 
     def setUp(self):
         self._dashboard_interface.start_robot()
