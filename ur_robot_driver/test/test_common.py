a26d149e7f5c981bc1bec2ab0267290cc1490ca2
Felix Exner <feex@universal-robots.com>
2025-10-10 12:31:29 +0200

    Add support for UR18 (#1524)
    (cherry picked from commit 233d524db55647a2aee5e03b7feb3948c7678464)
    
    # Conflicts:
    #	ur_moveit_config/launch/ur_moveit.launch.py
    #	ur_robot_driver/launch/ur_rsp.launch.py
    #	ur_robot_driver/urdf/ur.urdf.xacro

--- ur_robot_driver/test/test_common.py
+++ ur_robot_driver/test/test_common.py
@@ -326,6 +326,7 @@ def _declare_launch_arguments():
                 "ur16e",
                 "ur8long",
                 "ur15",
+                "ur18",
                 "ur20",
                 "ur30",
             ],
