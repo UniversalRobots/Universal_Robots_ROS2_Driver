5729f5c0d87ca251b1b195f870c2e8c7e0cebbc4
Felix Exner <feex@universal-robots.com>
2025-12-12 11:19:44 +0100

    [DashboardClient] Add a parameter callback (#1598)
    * Use sub-seconds for timeout configuration, as well
    
    Before, only the full seconds of the parameter were used. This commit
    also uses the non-integer part of the given timeout.
    
    For example, a timeout of 1.4 seconds before was configured as 1 sec.
    Now it is configured as 1 sec + 400000 us.
    
    * Add parameter callback to dashboard client
    
    Add a callback to change the receive_timeout parameter on the fly.
    
    * Add dashboard_receive_timeout parameter to launchfiles
    
    Also use the dashboard launchfile from the ur_control launchfile instead
    of starting the node directly. This way, it is possible to propagate the
    receive timeout to the ur_control launchfile.
    
    * Update documentation
    
    (cherry picked from commit 02d7fc6f8c7f212545cd03d0ca6eef13a1bcea9d)
    
    # Conflicts:
    #	ur_robot_driver/doc/dashboard_client.rst

--- ur_robot_driver/launch/ur_dashboard_client.launch.py
+++ ur_robot_driver/launch/ur_dashboard_client.launch.py
@@ -42,9 +42,17 @@ def generate_launch_description():
             description="IP address by which the robot can be reached.",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "dashboard_receive_timeout",
+            default_value="20.0",
+            description="Timeout that the dashboard client will wait for a response from the robot.",
+        )
+    )
 
     # Initialize Arguments
     robot_ip = LaunchConfiguration("robot_ip")
+    dashboard_receive_timeout = LaunchConfiguration("dashboard_receive_timeout")
 
     dashboard_client_node = Node(
         package="ur_robot_driver",
@@ -52,7 +60,10 @@ def generate_launch_description():
         name="dashboard_client",
         output="screen",
         emulate_tty=True,
-        parameters=[{"robot_ip": robot_ip}],
+        parameters=[
+            {"robot_ip": robot_ip},
+            {"receive_timeout": dashboard_receive_timeout},
+        ],
     )
 
     return LaunchDescription(declared_arguments + [dashboard_client_node])
