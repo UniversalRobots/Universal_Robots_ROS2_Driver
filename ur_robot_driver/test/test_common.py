57107718c0a0b30e1dbbf560f9cefd28550b8573
Felix Exner <feex@universal-robots.com>
2026-04-13 16:51:12 +0200

    REVERT_ME: Debug /ursim/programs

--- ur_robot_driver/test/test_common.py
+++ ur_robot_driver/test/test_common.py
@@ -579,7 +579,8 @@ def generate_driver_test_description(
     )
 
     inspect_ursim = ExecuteProcess(
-        cmd=["docker", "inspect", "ursim"],
+        # cmd=["docker", "inspect", "ursim"],
+        cmd=["docker", "exec", "ursim", "ls", "/ursim/programs"],
         name="inspect_ursim",
         output="screen",
     )
