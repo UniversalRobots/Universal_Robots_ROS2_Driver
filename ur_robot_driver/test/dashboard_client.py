d1e464667c58fb7ec4fbfa156b1e6060a97fa2fb
Felix Exner <feex@universal-robots.com>
2026-09-04 09:40:40 +0000

    Remove redundant check

--- ur_robot_driver/test/dashboard_client.py
+++ ur_robot_driver/test/dashboard_client.py
@@ -273,7 +273,7 @@ class DashboardClientTest(unittest.TestCase):
         self.assertNotEqual(resp.generated_file_name, "")
 
     def test_download_support_file(self, ursim_version):
-        if not _is_polyscope_x_at_least(ursim_version, "10.14.0") or ursim_version.startswith("5."):
+        if not _is_polyscope_x_at_least(ursim_version, "10.14.0"):
             self.skipTest("Downloading support files requires PolyScope X >= 10.14.0")
         target_path = "/tmp/ur_dashboard_support_files.zip"
         if os.path.exists(target_path):
