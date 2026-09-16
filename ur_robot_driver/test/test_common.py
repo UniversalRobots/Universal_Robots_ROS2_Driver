a4c90a992c4c008996bad95c487c693fc47aa313
Felix Exner <feex@universal-robots.com>
2026-09-16 16:54:19 +0200

    Currents as efforts (#1918)
    * Allow using the actual torques as joint efforts
    * Make adding this backwards compatible
    * Add documentation
    * Excplicitly fail when actual_current_as_torque is not available
    
    (cherry picked from commit f2817d7ec5993f45bf8bb6f40268c7c6a6d9ab10)
    
    # Conflicts:
    #	ur_robot_driver/doc/migration/lyrical.rst
    #	ur_robot_driver/doc/usage/force_torque_control.rst
    #	ur_robot_driver/doc/usage/startup.rst
    #	ur_robot_driver/doc/usage/utility_controllers.rst
    #	ur_robot_driver/src/hardware_interface.cpp
    #	ur_robot_driver/test/test_common.py
    #	ur_robot_driver/urdf/ur.ros2_control.xacro
    #	ur_robot_driver/urdf/ur.urdf.xacro

--- ur_robot_driver/test/test_common.py
+++ ur_robot_driver/test/test_common.py
@@ -650,6 +650,7 @@ def generate_driver_test_description(
     ur_type="ur5e",
     ursim_program_folder=None,
     urcap_folder=None,
+    use_currents_as_efforts=None,
 ):
     launch_arguments = {
         "robot_ip": "192.168.56.101",
@@ -661,6 +662,8 @@ def generate_driver_test_description(
         "launch_dashboard_client": "true",
         "start_joint_controller": "false",
     }
+    if use_currents_as_efforts is not None:
+        launch_arguments["use_currents_as_efforts"] = use_currents_as_efforts
     if tf_prefix:
         launch_arguments["tf_prefix"] = tf_prefix
 
@@ -703,6 +706,7 @@ def generate_driver_test_description_for_model(
     hw_name="ur",
     ursim_version="latest",
     ursim_type=None,
+    use_currents_as_efforts=None,
 ):
     """
     Generate a launch description that brings up URSim and the driver for an explicit ``ur_type``.
@@ -720,8 +724,33 @@ def generate_driver_test_description_for_model(
     if ursim_type is None:
         ursim_type = ur_type
 
+<<<<<<< HEAD
     description_launchfile = (
         PathJoinSubstitution([FindPackageShare("ur_robot_driver"), "launch", "ur_rsp.launch.py"]),
+=======
+    launch_arguments = {
+        "robot_ip": "192.168.56.101",
+        "ur_type": ur_type,
+        "launch_rviz": "false",
+        "controller_spawner_timeout": str(controller_spawner_timeout),
+        "initial_joint_controller": initial_joint_controller,
+        "headless_mode": "true",
+        "launch_dashboard_client": "true",
+        "start_joint_controller": "false",
+    }
+    if use_currents_as_efforts is not None:
+        launch_arguments["use_currents_as_efforts"] = use_currents_as_efforts
+    if tf_prefix:
+        launch_arguments["tf_prefix"] = tf_prefix
+
+    robot_driver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("ur_robot_driver"), "launch", "ur_control.launch.py"]
+            )
+        ),
+        launch_arguments=launch_arguments.items(),
+>>>>>>> f2817d7 (Currents as efforts (#1918))
     )
 
     rsp = IncludeLaunchDescription(
